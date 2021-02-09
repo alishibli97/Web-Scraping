0c51a4d1ebe4e06c523376ef860b7104c952f7fe
Ali Shibli <30579166+alishibli97@users.noreply.github.com>
2021-02-09 01:46:41 +0100

    Added Google meta-data + scraping base64 images

--- combine.py
+++ combine.py
@@ -9,33 +9,32 @@ import os
 import re
 import sys
 import argparse
-
+from datetime import datetime
+import base64
 
 class Image_Caption_Scraper():
 
-    def __init__(self, headless=True, chrome_binary=None, chrome_driver='chromedriver'):
+    def __init__(self,headless=True):
         """Initialization is only starting the web driver"""
-        self.start_web_driver(headless, chrome_binary, chrome_driver)
+        self.start_web_driver(headless)
 
-    def start_web_driver(self, headless, chrome_binary: str=None, chrome_driver='chromedriver'):
+    def start_web_driver(self,headless):
         """Create the webdriver and point it to the specific search engine"""
         chrome_options = Options()
-        chrome_options.headless = headless
-        if chrome_binary is not None:
-            chrome_options.binary_location = chrome_binary
-        self.wd = webdriver.Chrome(executable_path=chrome_driver, options=chrome_options)
+        if headless: chrome_options.add_argument("--headless")
+        self.wd = webdriver.Chrome(options=chrome_options)
 
     def scrape(self,engine,num_images,query):
         """Main function to scrape"""
         self.set_target_url(query,engine)
-        if engine=='google': img_caption = self.get_google_images(query,num_images)
-        elif engine=='yahoo': img_caption = self.get_yahoo_images(query,num_images)
-        elif engine=='flickr': img_caption = self.get_flickr_images(query,num_images)
+        if engine=='google': img_data = self.get_google_images(query,num_images)
+        elif engine=='yahoo': img_data = self.get_yahoo_images(query,num_images)
+        elif engine=='flickr': img_data = self.get_flickr_images(query,num_images)
 
         self.engine = engine
         self.query = query
 
-        return img_caption
+        return img_data
 
     def set_target_url(self,query,engine):
         """Given the target engine and query, build the target url"""
@@ -56,11 +55,11 @@ class Image_Caption_Scraper():
     def get_google_images(self,query,num_images):
         """Retrieve urls for images and captions from Google Images search engine"""
         self.wd.get(self.target_url)   
-        img_caption = {}
+        img_data = {}
 
         start = 0
         prevLength = 0
-        while(len(img_caption)<num_images):
+        while(len(img_data)<num_images):
             self.scroll_to_end();i=0
 
             thumbnail_results = self.wd.find_elements_by_css_selector("img.Q4LuWd")
@@ -75,25 +74,36 @@ class Image_Caption_Scraper():
                     self.wd.execute_script("arguments[0].click();", content)
                     time.sleep(0.5)
 
-                    caption = self.wd.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[3]/div[2]/a').text
+                    caption = self.wd.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[3]/div[2]/a').text
+
                     url = self.wd.find_elements_by_css_selector('img.n3VNCb')[0]
-                    # for url in urls:
-                    if url.get_attribute('src') and 'http' in url.get_attribute('src') and not url.get_attribute('src').endswith('gif') and url.get_attribute('src') not in img_caption:
-                        img_caption[url.get_attribute('src')]=caption
-                        print(f"Finished {len(img_caption)} images.")
+
+                    if url.get_attribute('src') and not url.get_attribute('src').endswith('gif') and url.get_attribute('src') not in img_data:
+
+                        now = datetime.now().astimezone()
+                        now = now.strftime("%m-%d-%Y %H:%M:%S %z %Z")
+
+                        img_data[len(img_data)]={
+                            'url':url.get_attribute('src'),
+                            'caption':caption,
+                            'datetime': now,
+                            'source': 'google'
+                        }
+
+                        print(f"Finished {len(img_data)} images.")
                 except:
                     print("Couldn't load image/caption")
-                if(len(img_caption)>num_images-1): break
+                if(len(img_data)>num_images-1): break
             start = len(thumbnail_results)
 
-        return img_caption
+        return img_data
 
     def get_yahoo_images(self,query,num_images):
         """Retrieve urls for images and captions from Yahoo Images search engine"""
         self.wd.get(self.target_url)
         time.sleep(3)
         # self.wd.delete_all_cookies()
-        img_caption = {}
+        img_data = {}
 
         # button = self.wd.find_element_by_name('more-res')
 
@@ -105,7 +115,7 @@ class Image_Caption_Scraper():
         prevLength = 0
         enter = 0
         i=0
-        while(len(img_caption)<num_images):
+        while(len(img_data)<num_images):
             self.scroll_to_end()
             # scroll_to_end_yahoo()
 
@@ -133,22 +143,22 @@ class Image_Caption_Scraper():
                 caption = self.wd.find_element_by_class_name('title').text
 
                 url = self.wd.find_element_by_xpath('//*[@id="img"]')
-                if url.get_attribute('src') and 'http' in url.get_attribute('src') and not url.get_attribute('src').endswith('gif') and url.get_attribute('src') not in img_caption:
-                    img_caption[url.get_attribute('src')]=caption
-                    print(f"Finished {len(img_caption)} images.")
-                if(len(img_caption)>num_images-1): break
+                if url.get_attribute('src') and 'http' in url.get_attribute('src') and not url.get_attribute('src').endswith('gif') and url.get_attribute('src') not in img_data:
+                    img_data[url.get_attribute('src')]=caption
+                    print(f"Finished {len(img_data)} images.")
+                if(len(img_data)>num_images-1): break
             start = len(items)
-        return img_caption
+        return img_data
         
     def get_flickr_images(self,query,num_images):
         """Retrieve urls for images and captions from Flickr Images search engine"""
         self.wd.get(self.target_url)
-        img_caption = {}
+        img_data = {}
 
         start = 0
         prevLength = 0
         waited = False
-        while(len(img_caption)<num_images):
+        while(len(img_data)<num_images):
             self.scroll_to_end()
             # scroll_to_end_flickr()
 
@@ -170,13 +180,13 @@ class Image_Caption_Scraper():
                     url = "http://"+url.group(1)
                     caption = item.find_element_by_class_name('interaction-bar').get_attribute('title')
                     caption = caption[:re.search(r'\bby\b',caption).start()].strip()
-                    img_caption[url]=caption
-                    print(f"Finished {len(img_caption)} images.")
-                if(len(img_caption)>num_images-1): break
+                    img_data[url]=caption
+                    print(f"Finished {len(img_data)} images.")
+                if(len(img_data)>num_images-1): break
             start = len(items)
-        return img_caption
+        return img_data
 
-    def save_pictures_captions(self,img_caption,out_dir):
+    def save_pictures_captions(self,img_data,out_dir):
         """Retrieve the images and save them in directory with the captions"""
         query = '_'.join(self.query.lower().split())
 
@@ -185,34 +195,43 @@ class Image_Caption_Scraper():
         if not os.path.exists(target_folder):
             os.makedirs(target_folder)
 
-        # target_folder = os.path.join(f'./{out_dir}/{self.engine}', query)
-        # if not os.path.exists(target_folder):
-        #     os.makedirs(target_folder)
-
-        for i,(url,val) in enumerate(img_caption.items()):
+        for i,val in enumerate(img_data.values()):
             print("Saving image",i)
             try:
-                img_content = requests.get(url).content
-                img_file = io.BytesIO(img_content)
-                img = Image.open(img_file).convert('RGB')
-                file_path = os.path.join(f'{self.engine}/{query}/{i}.jpg')
-                f = open(file_path,"wb")
-                img.save(f,"JPEG",quality=95)
+                url = val['url']
+
+                if(url.startswith('http')):
+                    img_content = requests.get(url).content
+                    img_file = io.BytesIO(img_content)
+                    img = Image.open(img_file).convert('RGB')
+                    file_path = os.path.join(f'{self.engine}/{query}/{i}.jpg')
+                    with open(file_path,"wb") as f:
+                        img.save(f,"JPEG",quality=95)
+                        # print("Saved using http")
+                
+                elif(url.startswith('data')):
+                    base64_img = url.split(',')[1]
+                    base64_img_bytes = base64_img.encode('utf-8')
+                    file_path = os.path.join(f'{self.engine}/{query}/{i}.jpg')
+                    with open(file_path, 'wb') as f:
+                        decoded_image_data = base64.decodebytes(base64_img_bytes)
+                        f.write(decoded_image_data)
+                    # print("Saved using base64")
+
             except:
                 print("Couldn't save image")
 
-        img_caption = {f'{i}.jpg': val for i,val in enumerate(img_caption.values())}
+        # img_data = {f'{i}.jpg': val for i,val in enumerate(img_data.values())}
         file_path = f'{self.engine}/{query}/{query}.json'
         with open(file_path, 'w+') as fp:
-            json.dump(img_caption, fp)
+            json.dump(img_data, fp)
         print("Saved urls file at:",os.path.join(os.getcwd(),file_path))
 
-    def save_json_URLS(self,img_caption,out_dir):
-        """Save only the urls with the captions without the images"""
-        img_caption = {f'{i}.jpg': key for i,key in enumerate(img_caption.keys())}
+    def save_json_URLS(self,img_data,out_dir):
+        """Save only the meta data without the images"""
         file_path = f'{out_dir}/{self.engine}/{self.query}/{self.query}_urls.json'
         with open(file_path, 'w+') as fp:
-            json.dump(img_caption, fp)
+            json.dump(img_data, fp)
         print("Saved urls file at:",os.path.join(os.getcwd(),file_path))
 
 if __name__ == "__main__":
@@ -221,18 +240,16 @@ if __name__ == "__main__":
     parser.add_argument('--num_images',required=True,type=int)
     parser.add_argument('--query',required=True,type=str)
     parser.add_argument('--out_dir',type=str,default='images')
-    parser.add_argument('--headless', action='store_true')
-    parser.add_argument('--chrome-binary', type=str)
-    parser.add_argument('--chrome-driver', type=str)
+    parser.add_argument('headless',type=str, nargs='?')
     args = parser.parse_args()
 
     engine = args.engine
     num_images = args.num_images
     query = args.query
     out_dir = args.out_dir
-    headless = args.headless
+    headless = args.headless is not None
     
-    scraper = Image_Caption_Scraper(headless, args.chrome_binary, args.chrome_driver)
+    scraper = Image_Caption_Scraper(headless)
 
-    img_caption = scraper.scrape(engine,num_images,query)
-    scraper.save_pictures_captions(img_caption,out_dir)
+    img_data = scraper.scrape(engine,num_images,query)
+    scraper.save_pictures_captions(img_data,out_dir)
