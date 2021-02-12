da1e3a3df55b25611c4aaa5392034bb8ceef915c
Federico Baldassarre <baldassarre.fe@gmail.com>
2021-02-12 18:59:05 +0100

    Set random User-Agent when downloading images to avoid '403 Forbidden'

--- pipeline/scraper.py
+++ pipeline/scraper.py
@@ -4,6 +4,7 @@ import argparse
 import base64
 import io
 import json
+import random
 import re
 import sys
 import time
@@ -259,6 +260,16 @@ class ScrapingDict(TypedDict, total=False):
 
 
 def download_image(img_dict: Mapping[str, Any], output_dir: Union[str, Path]):
+    user_agents = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Mobile Safari/537.36",
+        "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+    ]
+
     path = (
         Path(output_dir)
         .joinpath(img_dict["engine"])
@@ -269,12 +280,12 @@ def download_image(img_dict: Mapping[str, Any], output_dir: Union[str, Path]):
     path.parent.mkdir(parents=True, exist_ok=True)
 
     if img_dict["url"].startswith("http"):
-        response = requests.get(img_dict["url"])
+        response = requests.get(
+            img_dict["url"], headers={"User-Agent": random.choice(user_agents)}
+        )
         response.raise_for_status()
         img = Image.open(io.BytesIO(response.content))
-        try: 
-            img = img.convert("RGB")
-        except: pass
+        img = img.convert("RGB")
         with path.open("wb") as f:
             img.save(f, "JPEG", quality=95)
     elif img_dict["url"].startswith("data"):
